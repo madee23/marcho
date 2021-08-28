44e57f814ef38cab88ca384ef196d264aa78a728
madee23 <madee23@mail.ru>
2021-08-28 13:48:20 +0700

    added top-slider section[B

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ function browsersync() {
 // Тасчим ЭСЦСС
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'expanded' }))
+    .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 version'],
@@ -38,6 +38,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
